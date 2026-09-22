690265fc00f4c5bb12425a0c2db470d2c6d13212
talestavarpidde <tales.pidde@estudante.ifgoiano.edu.br>
2026-09-22 20:42:30 -0300

    correção de formatação

--- codigo/cad_postagem.php
+++ codigo/cad_postagem.php
@@ -1,7 +1,6 @@
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -10,7 +9,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
 </head>
-
 <body>
     <div>
         <form action="salvarPostagem.php" method="POST">
